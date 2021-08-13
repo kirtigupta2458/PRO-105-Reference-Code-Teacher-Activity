97c5671d68ececbf3f9448ed4266c1b98814f433
procodingclass <88050205+procodingclass@users.noreply.github.com>
2021-08-13 12:11:25 +0530

    Add files via upload

---
+++ Read_Video.py
@@ -0,0 +1,32 @@
+import cv2
+
+vid = cv2.VideoCapture("AnthonyShkraba.mp4")
+
+if(vid.isOpened()==False):
+    print("Unable to read the feed")
+
+
+height  = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(height)
+width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(width)
+fps = int(vid.get(cv2.CAP_PROP_FPS))
+print(fps)
+
+out = cv2.VideoWriter('Boxing.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 30, (width,height))
+
+while(True):
+      
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+
+    cv2.imshow("Web cam", frame)
+    out.write(frame)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+vid.release()
+out.release()
+
+cv2.destroyAllWindows()
